ef71ea4921b672c66e105e34e7a47c94221c6e0d
Leon Moraes <36496722+lmoraes95@users.noreply.github.com>
2022-11-08 12:52:09 -0800

    new files

--- page/Webpage/templates/heatmap.js
+++
@@ -1,3 +0,0 @@
-fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=WSWHDLRSUMO2PIET')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
